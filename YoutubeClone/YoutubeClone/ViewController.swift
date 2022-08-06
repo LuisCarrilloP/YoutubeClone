8eaa3699c2182b3bc375e9886c81357f62613c21
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-08-05 22:22:07 -0500

    Set colors and preview image

---
+++ YoutubeClone/YoutubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by José Luis Carrillo Padrón on 05/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
